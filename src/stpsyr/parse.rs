95b950559a52610b1a4746514d8f07cb5794905a
KeyboardFire <andy@keyboardfire.com>
2017-07-10 19:59:51 -0500

    Parse retreats and adjusts
    These could... use some work.
    
    Anyway, we're now fully capable of playing 1901, which is neat.

--- src/stpsyr/parse.rs
+++ src/stpsyr/parse.rs
@@ -44,15 +44,13 @@ impl Stpsyr {
                 ).map(|&token| match token {
                     "support" | "supports" => "s",
                     "convoy" | "convoys" | "vc" => "c",
-                    "north" => "nc",
-                    "south" => "sc",
-                    "east" => "ec",
-                    "west" => "wc",
                     _ => token
                 });
+
             let province = if let Some(p) = tokens_iter.next() {
                 Province::from(p)
             } else { continue };
+
             match tokens_iter.next() {
                 None => {}, // hold
                 Some(token2) => { match token2 {
@@ -95,11 +93,74 @@ impl Stpsyr {
     }
 
     fn parse_retreats(&mut self, power: &Power, orders: String) {
-        unimplemented!();
+        for line in orders.lines() {
+            let line = line.to_lowercase()
+                .replace('(', "/")
+                .replace(" /", "/")
+                .replace("/ ", "/");
+            let tokens: Vec<&str> = line
+                .split(|c: char| !(c.is_lowercase() || c == '/') )
+                .collect();
+            let mut tokens_iter = tokens.iter().filter(|&token|
+                    token.len() >= 3 &&
+                    *token != "army" && *token != "fleet" &&
+                    *token != "move" && *token != "moves" &&
+                    *token != "retreat" && *token != "retreats" &&
+                    *token != "disband" && *token != "disbands" &&
+                    *token != "the" && *token != "coast"
+                );
+
+            let p1 = if let Some(p) = tokens_iter.next() {
+                Province::from(*p)
+            } else { continue };
+
+            if let Some(p2) = tokens_iter.next() {
+                if tokens_iter.next().is_some() { continue; }
+                self.add_retreat(power.clone(), p1,
+                        RetreatAction::Move { to: Province::from(*p2) });
+            } else {
+                self.add_retreat(power.clone(), p1, RetreatAction::Disband);
+            }
+        }
     }
 
     fn parse_adjusts(&mut self, power: &Power, orders: String) {
-        unimplemented!();
+        for line in orders.lines() {
+            let line = line.to_lowercase()
+                .replace('(', "/")
+                .replace(" /", "/")
+                .replace("/ ", "/");
+            let tokens: Vec<&str> = line
+                .split(|c: char| !(c.is_lowercase() || c == '/') )
+                .collect();
+            let mut tokens_iter = tokens.iter().filter(|&token|
+                    (token.len() >= 3 ||
+                     *token == "d" || *token == "a" || *token == "f") &&
+                    *token != "build" &&
+                    *token != "the" && *token != "coast"
+                ).map(|&token| match token {
+                    "destroy" => "d",
+                    "army" => "a",
+                    "fleet" => "f",
+                    _ => token
+                });
+
+            match tokens_iter.next() {
+                Some("d") => if let Some(p) = tokens_iter.next() {
+                    self.add_adjust(power.clone(), Province::from(p),
+                            AdjustAction::Disband);
+                },
+                Some("a") => if let Some(p) = tokens_iter.next() {
+                    self.add_adjust(power.clone(), Province::from(p),
+                            AdjustAction::Build { unit_type: UnitType::Army });
+                },
+                Some("f") => if let Some(p) = tokens_iter.next() {
+                    self.add_adjust(power.clone(), Province::from(p),
+                            AdjustAction::Build { unit_type: UnitType::Fleet });
+                },
+                _ => {} // invalid
+            }
+        }
     }
 
 }
